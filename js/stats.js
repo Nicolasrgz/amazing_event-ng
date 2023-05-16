58be4393b8851f0cd75352da317bcfe01f7c1118
chaquito7 <nicolsgonza765@gmail.com>
2023-05-16 12:27:08 -0300

    version API 1.3

--- js/stats.js
+++ js/stats.js
@@ -1,42 +1,87 @@
-const table = document.getElementById('table')
+const table1 = document.getElementById('table-1');
+const table2 = document.getElementById('table-2')
+const table3 = document.getElementById('table-3')
 
-
-let apiEvents
+let apiEvents;
 fetch("https://mindhub-xj03.onrender.com/api/amazing")
-.then(res => res.json())
-.then(data => {
-  apiEvents = data
-    printTable(apiEvents.events, table)  
+  .then(res => res.json())
+  .then(data => {
+    apiEvents = data;
+    const eventoMayorAsistencia = calcularPorcentajeMayor(apiEvents.events.map(event => event.name), apiEvents.events.map(event => event.assistance), apiEvents.events.map(event => event.capacity));
+    const eventoMenorAsistencia = calcularMenorPorcentaje(apiEvents.events.map(event => event.name), apiEvents.events.map(event => event.assistance), apiEvents.events.map(event => event.capacity));
+    const eventoConMayorCapacidad = mayorAsistencia(apiEvents.events.map(event => event.name), apiEvents.events.map(event => event.capacity));
+  
+    estructuraTable1(eventoMayorAsistencia, eventoMenorAsistencia, eventoConMayorCapacidad);
+  });
+  
+
+  function calcularPorcentajeMayor(name, asistencia, capacidad) {
+    let arrayDeDatos = [];
+    for (let i = 0; i < apiEvents.events.length; i++) {
+      if (!isNaN(asistencia[i]) && !isNaN(capacidad[i])) {
+        let porcentaje = (asistencia[i] * 100) / capacidad[i];
+        let elemento = { name: name[i], porcentaje: porcentaje };
+        arrayDeDatos.push(elemento);
+      }}
     
-})
+    const eventoMayorAsistencia = arrayDeDatos.reduce((max, current) => {
+      return current.porcentaje > max.porcentaje ? current : max;
+    });
+    
+  return ((eventoMayorAsistencia.name + " : " + eventoMayorAsistencia.porcentaje.toFixed(2) + "%"))
+  }
+  
 
-function armarTable (obj){
-    return `                
-    <thead id="thead">
-    <tr>
-    <th>Event with the highest percentage of attendance</th>
-    <th>Event with the lowest percentage of attendance</th>
-    <th>Event with larger capacity</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-    </tr>
-    <tr>
-      <td class="td-op">-</td>
-      <td class="td-op">-</td>
-      <td class="td-op">-</td>
-    </tr>
-  </tbody>`
-}
 
-function printTable (data, ubicacion){
-    for (let elemento of data){
-        ubicacion.innerHTML = armarTable(elemento)
+  function calcularMenorPorcentaje(name, asistencia, capacidad) {
+    let arrayDeDatos = [];
+    for (let i = 0; i < apiEvents.events.length; i++) {
+      if (!isNaN(asistencia[i]) && !isNaN(capacidad[i])) {
+        let porcentaje = (asistencia[i] * 100) / capacidad[i];
+        let elemento = { name: name[i], porcentaje: porcentaje };
+        arrayDeDatos.push(elemento);
+      }
     }
-}
+    
+    const eventoMenorAsistencia = arrayDeDatos.reduce((min, current) => {
+      return current.porcentaje < min.porcentaje ? current : min;
+    });
+    
+     return (eventoMenorAsistencia.name + ": " + eventoMenorAsistencia.porcentaje.toFixed(2) + "%");
+    
+  }
+  
+  function mayorAsistencia(name, capacidad) {
+    let arrayDeDatos = [];
+    for (let i = 0; i < apiEvents.events.length; i++) {
+      if (capacidad[i]) {
+        let elemento = { name: name[i], capacity: capacidad[i] };
+        arrayDeDatos.push(elemento);
+      }
+    }
+    
+    const mayorCapacidad = arrayDeDatos.sort((a, b) => b.capacity - a.capacity);
+    if (mayorCapacidad.length > 0) {
+      const eventoConMayorCapacidad = mayorCapacidad[0];
+      return (eventoConMayorCapacidad.name + ": " + eventoConMayorCapacidad.capacity);
+    }
+  }
 
-function assistanceEvents (name, capacidad, assistance){
-    return `${name} su porcentaje de asistencia fue de ${(assistance * 100)/capacidad}`  
-}
-
+  function estructuraTable1(eventoMayorAsistencia, eventoMenorAsistencia, mayorCapacidad) {
+    let estructura = `
+      <tr>
+        <th>Event with the highest % of attendance</th>
+        <th>Event with the lowest % of attendance</th>
+        <th>Event with larger capacity</th>
+      </tr>
+      <tr>
+        <td>${eventoMayorAsistencia}</td>
+        <td>${eventoMenorAsistencia}</td>
+        <td>${mayorCapacidad}</td>
+      </tr>
+    `;
+    
+    // Insertar la estructura en la tabla
+    table1.innerHTML = estructura;
+  }
+  
