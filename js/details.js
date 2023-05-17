66992a1fe2c597da43736097af93e2461b12c5f1
chaquito7 <nicolsgonza765@gmail.com>
2023-05-17 15:15:55 -0300

    version API 1.4.2

--- js/details.js
+++ js/details.js
@@ -20,7 +20,7 @@ fetch("https://mindhub-xj03.onrender.com/api/amazing")
   <h4 class="h5-p">date: ${eventDetails.date}5</h4>
   <h5 class="h5-p"> ${eventDetails.description}</h5>
   <h4 class="h5-p">category: ${eventDetails.category}</h4>
-  <h5 class="h5-p">capacity:${eventDetails.capacity}</h5>
+  <h5 class="h5-p">capacity: ${eventDetails.capacity}</h5>
   <h3 class="h5-p">price: ${eventDetails.price}</h3>
  </div>
  </div>`
